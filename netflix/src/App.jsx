cf42c0153af7b9264db13af31014726ae6b86db2
Devanshugoell <devgoyal952@gmail.com>
2026-09-20 14:07:05 +0530

    fixed tailwind css issue

---
+++ netflix/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-2xl text-amber-600 text-center'>This is heading</h1>
+    </div>
+  );
+}
+
+export default App;
